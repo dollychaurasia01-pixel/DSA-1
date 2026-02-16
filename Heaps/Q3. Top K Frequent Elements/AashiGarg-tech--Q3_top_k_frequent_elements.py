fc325aa18ff9c37060a314649d4c412bd62e0ebd
Aashi Garg <aashigarg2504@gmail.com>
2026-02-17 00:34:27 +0530

    Heaps - Q4 - Top K frequent elements - python

---
+++ Heaps/Q3. Top K Frequent Elements/AashiGarg-tech--Q3_top_k_frequent_elements.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        from collections import Counter
+        import heapq
+
+        freq = Counter(nums)
+
+        return [item for item, _ in heapq.nlargest(k, freq.items(), key=lambda x: x[1])]
